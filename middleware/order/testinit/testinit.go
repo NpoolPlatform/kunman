3cc1af8a01c5f00dd1515f7bbfe182462d1904ae
Zhao KK <kikakkz@hotmail.com>
2025-06-07 08:34:49 +0000

    Move testinit to common place

--- middleware/order/testinit/testinit.go
+++ middleware/order/testinit/testinit.go
@@ -1,44 +1,10 @@
 package testinit
 
 import (
-	"fmt"
-	"path"
-	"runtime"
-
-	"github.com/NpoolPlatform/kunman/framework/app"
-
 	myservicename "github.com/NpoolPlatform/kunman/middleware/order/servicename"
-	servicename "github.com/NpoolPlatform/kunman/pkg/servicename"
-
-	mysqlconst "github.com/NpoolPlatform/kunman/framework/mysql/const"
-	rabbitmqconst "github.com/NpoolPlatform/kunman/framework/rabbitmq/const"
-	redisconst "github.com/NpoolPlatform/kunman/framework/redis/const"
+	testinit "github.com/NpoolPlatform/kunman/pkg/testinit"
 )
 
 func Init() error {
-	_, myPath, _, ok := runtime.Caller(0)
-	if !ok {
-		return fmt.Errorf("cannot get source file path")
-	}
-
-	configPath := fmt.Sprintf("%s/../../../cmd/kunman", path.Dir(myPath))
-
-	err := app.Initialize(
-		servicename.ServiceName,
-		"",
-		"",
-		"",
-		configPath,
-		nil,
-		nil,
-		myservicename.ServiceDomain,
-		mysqlconst.MysqlServiceName,
-		rabbitmqconst.RabbitMQServiceName,
-		redisconst.RedisServiceName,
-	)
-	if err != nil {
-		return fmt.Errorf("cannot init app stub: %v", err)
-	}
-
-	return nil
+	return testinit.Initialize(myservicename.ServiceDomain)
 }
---
+++ pkg/testinit/testinit.go
@@ -0,0 +1,43 @@
+package testinit
+
+import (
+	"fmt"
+	"path"
+	"runtime"
+
+	"github.com/NpoolPlatform/kunman/framework/app"
+
+	servicename "github.com/NpoolPlatform/kunman/pkg/servicename"
+
+	mysqlconst "github.com/NpoolPlatform/kunman/framework/mysql/const"
+	rabbitmqconst "github.com/NpoolPlatform/kunman/framework/rabbitmq/const"
+	redisconst "github.com/NpoolPlatform/kunman/framework/redis/const"
+)
+
+func Initialize(serviceName string) error {
+	_, myPath, _, ok := runtime.Caller(0)
+	if !ok {
+		return fmt.Errorf("cannot get source file path")
+	}
+
+	configPath := fmt.Sprintf("%s/../../cmd/kunman", path.Dir(myPath))
+
+	err := app.Initialize(
+		servicename.ServiceName,
+		"",
+		"",
+		"",
+		configPath,
+		nil,
+		nil,
+		serviceName,
+		mysqlconst.MysqlServiceName,
+		rabbitmqconst.RabbitMQServiceName,
+		redisconst.RedisServiceName,
+	)
+	if err != nil {
+		return fmt.Errorf("cannot init app stub: %v", err)
+	}
+
+	return nil
+}
