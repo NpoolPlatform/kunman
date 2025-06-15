a4a233333f2f0f8f9d16e9a682469196c146690b
Zhao KK <kikakkz@hotmail.com>
2025-06-15 09:59:36 +0000

    Add unit test to subscription

---
+++ gateway/order/subscription/subscription_test.go
@@ -0,0 +1,205 @@
+package subscription
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strconv"
+	"testing"
+
+	paymentgwpb "github.com/NpoolPlatform/kunman/message/order/gateway/v1/payment"
+	ordercouponmwpb "github.com/NpoolPlatform/kunman/message/order/middleware/v1/order/coupon"
+	paymentmwpb "github.com/NpoolPlatform/kunman/message/order/middleware/v1/payment"
+	npool "github.com/NpoolPlatform/kunman/message/order/middleware/v1/subscription"
+	appsubscription1 "github.com/NpoolPlatform/kunman/middleware/good/app/subscription"
+	subscription1 "github.com/NpoolPlatform/kunman/middleware/good/subscription"
+
+	"github.com/google/uuid"
+	"github.com/shopspring/decimal"
+	"github.com/stretchr/testify/assert"
+
+	goodtypes "github.com/NpoolPlatform/kunman/message/basetypes/good/v1"
+	types "github.com/NpoolPlatform/kunman/message/basetypes/order/v1"
+	"github.com/NpoolPlatform/kunman/middleware/good/testinit"
+)
+
+func init() {
+	if runByGithubAction, err := strconv.ParseBool(os.Getenv("RUN_BY_GITHUB_ACTION")); err == nil && runByGithubAction {
+		return
+	}
+	if err := testinit.Init(); err != nil {
+		fmt.Printf("cannot init test stub: %v\n", err)
+	}
+}
+
+var ret = npool.SubscriptionOrder{
+	EntID:               uuid.NewString(),
+	AppID:               uuid.NewString(),
+	UserID:              uuid.NewString(),
+	GoodID:              uuid.NewString(),
+	GoodType:            goodtypes.GoodType_Subscription,
+	AppGoodID:           uuid.NewString(),
+	OrderID:             uuid.NewString(),
+	OrderType:           types.OrderType_Normal,
+	PaymentType:         types.PaymentType_PayWithBalanceOnly,
+	CreateMethod:        types.OrderCreateMethod_OrderCreatedByPurchase,
+	GoodValueUSD:        decimal.NewFromInt(120).String(),
+	PaymentGoodValueUSD: decimal.NewFromInt(120).String(),
+	PaymentAmountUSD:    decimal.NewFromInt(110).String(),
+	DiscountAmountUSD:   decimal.NewFromInt(10).String(),
+	PromotionID:         uuid.NewString(),
+	DurationSeconds:     100000,
+	LedgerLockID:        uuid.NewString(),
+	PaymentID:           uuid.NewString(),
+	Coupons: []*ordercouponmwpb.OrderCouponInfo{
+		{
+			CouponID: uuid.NewString(),
+		},
+	},
+	PaymentBalances: []*paymentmwpb.PaymentBalanceInfo{
+		{
+			CoinTypeID:           uuid.NewString(),
+			Amount:               decimal.NewFromInt(110).String(),
+			CoinUSDCurrency:      decimal.NewFromInt(1).String(),
+			LocalCoinUSDCurrency: decimal.NewFromInt(1).String(),
+			LiveCoinUSDCurrency:  decimal.NewFromInt(1).String(),
+		},
+	},
+	OrderState:   types.OrderState_OrderStateCreated,
+	PaymentState: types.PaymentState_PaymentStateWait,
+}
+
+func setup(t *testing.T) func(*testing.T) {
+	ret.GoodTypeStr = ret.GoodType.String()
+
+	goodName := uuid.NewString()
+	appGoodName := uuid.NewString()
+	usdPrice := "12.99"
+	durationUnits := uint32(1)
+	durationQuota := uint32(2000)
+
+	subscriptionEntID := uuid.NewString()
+	h1, err := subscription1.NewHandler(
+		context.Background(),
+		subscription1.WithEntID(&subscriptionEntID, true),
+		subscription1.WithGoodID(&ret.GoodID, true),
+		subscription1.WithGoodType(&ret.GoodType, true),
+		subscription1.WithName(&goodName, true),
+		subscription1.WithUSDPrice(&usdPrice, true),
+		subscription1.WithDurationDisplayType(goodtypes.GoodDurationType_GoodDurationByWeek.Enum(), true),
+		subscription1.WithDurationUnits(&durationUnits, true),
+		subscription1.WithDurationQuota(&durationQuota, true),
+	)
+	assert.Nil(t, err)
+
+	err = h1.CreateSubscription(context.Background())
+	assert.Nil(t, err)
+
+	appSubscriptionEntID := uuid.NewString()
+	h2, err := appsubscription1.NewHandler(
+		context.Background(),
+		appsubscription1.WithEntID(&appSubscriptionEntID, true),
+		appsubscription1.WithAppID(&ret.AppID, true),
+		appsubscription1.WithGoodID(&ret.GoodID, true),
+		appsubscription1.WithAppGoodID(&ret.AppGoodID, true),
+		appsubscription1.WithName(&appGoodName, true),
+		appsubscription1.WithUSDPrice(&usdPrice, true),
+	)
+	assert.Nil(t, err)
+
+	err = h2.CreateSubscription(context.Background())
+	assert.Nil(t, err)
+
+	return func(*testing.T) {
+		_ = h2.DeleteSubscription(context.Background())
+		_ = h1.DeleteSubscription(context.Background())
+	}
+}
+
+func createSubscription(t *testing.T) {
+	paymentTransferCoinTypeID := uuid.NewString()
+	paymentFiatID := uuid.NewString()
+
+	handler, err := NewHandler(
+		context.Background(),
+		WithEntID(&ret.EntID, true),
+		WithAppID(&ret.AppID, true),
+		WithUserID(&ret.UserID, true),
+		WithGoodID(&ret.GoodID, true),
+		WithAppGoodID(&ret.AppGoodID, true),
+		WithOrderID(&ret.OrderID, true),
+		WithPaymentBalances(func() (balances []*paymentgwpb.PaymentBalance) {
+			for _, balance := range ret.PaymentBalances {
+				balances = append(balances, &paymentgwpb.PaymentBalance{
+					CoinTypeID: balance.CoinTypeID,
+					Amount:     balance.Amount,
+				})
+			}
+			return
+		}(), true),
+		WithPaymentTransferCoinTypeID(&paymentTransferCoinTypeID, true),
+		WithPaymentFiatID(&paymentFiatID, true),
+		WithCouponIDs(func() (couponIDs []string) {
+			for _, coupon := range ret.Coupons {
+				couponIDs = append(couponIDs, coupon.CouponID)
+			}
+			return
+		}(), true),
+		WithUserSetCanceled(&ret.UserSetCanceled, true),
+		WithAdminSetCanceled(&ret.AdminSetCanceled, true),
+		WithCreateMethod(&ret.CreateMethod, true),
+		WithOrderType(&ret.OrderType, true),
+	)
+	assert.Nil(t, err)
+
+	info, err := handler.CreateSubscriptionOrder(context.Background())
+	if assert.Nil(t, err) {
+		ret.CreatedAt = info.CreatedAt
+		ret.UpdatedAt = info.UpdatedAt
+		ret.ID = info.ID
+		assert.Equal(t, info, &ret)
+	}
+}
+
+func updateSubscription(t *testing.T) {
+}
+
+func getSubscription(t *testing.T) {
+	handler, err := NewHandler(
+		context.Background(),
+		WithID(&ret.ID, true),
+		WithEntID(&ret.EntID, true),
+		WithAppGoodID(&ret.AppGoodID, true),
+	)
+	assert.Nil(t, err)
+
+	info, err := handler.GetSubscriptionOrder(context.Background())
+	if assert.Nil(t, err) {
+		assert.Equal(t, info, &ret)
+	}
+}
+
+func existSubscription(t *testing.T) {
+}
+
+func getSubscriptions(t *testing.T) {
+}
+
+func deleteSubscription(t *testing.T) {
+}
+
+func TestSubscription(t *testing.T) {
+	if runByGithubAction, err := strconv.ParseBool(os.Getenv("RUN_BY_GITHUB_ACTION")); err == nil && runByGithubAction {
+		return
+	}
+
+	teardown := setup(t)
+	defer teardown(t)
+
+	t.Run("createSubscription", createSubscription)
+	t.Run("updateSubscription", updateSubscription)
+	t.Run("getSubscription", getSubscription)
+	t.Run("existSubscription", existSubscription)
+	t.Run("getSubscriptions", getSubscriptions)
+	t.Run("deleteSubscription", deleteSubscription)
+}
--- go.mod
+++ go.mod
@@ -6,11 +6,10 @@ require (
 	bou.ke/monkey v1.0.2
 	entgo.io/ent v0.14.4
 	github.com/AmirSoleimani/VoucherCodeGenerator/vcgen v0.0.0-20220706103434-bcb25b5bff9c
-	github.com/NpoolPlatform/account-middleware v0.0.0-20250115034541-5f3f60ceee9a
+	github.com/NpoolPlatform/go-service-framework v0.0.0-20250123031703-66a23c81e8e0
 	github.com/NpoolPlatform/good-gateway v0.0.0-20250303022721-b1e88c6dc0af
 	github.com/NpoolPlatform/good-middleware v0.0.0-20250115034449-9d9d053987a2
-	github.com/NpoolPlatform/message v0.0.0-20250604112546-25213bae6bb9
-	github.com/NpoolPlatform/miningpool-middleware v0.0.0-20241106020300-03d147aeb913
+	github.com/NpoolPlatform/message v0.0.0-20250115023515-4bc3d3d37f89
 	github.com/NpoolPlatform/order-middleware v0.0.0-20241106020324-b91339b3d20e
 	github.com/NpoolPlatform/sphinx-proxy v0.0.0-20231020115438-bd66d3d334af
 	github.com/ThreeDotsLabs/watermill v1.2.0
@@ -55,11 +54,10 @@ require (
 
 require (
 	ariga.io/atlas v0.31.1-0.20250212144724-069be8033e83 // indirect
-	entgo.io/contrib v0.6.1-0.20250319063240-8a7f182943cb // indirect
 	github.com/NpoolPlatform/appuser-middleware v0.0.0-20240926060520-1949246cf711 // indirect
 	github.com/NpoolPlatform/chain-middleware v0.0.0-20240731030645-1b4ce8fcac22 // indirect
-	github.com/NpoolPlatform/go-service-framework v0.0.0-20250123031703-66a23c81e8e0 // indirect
 	github.com/NpoolPlatform/libent-cruder v0.0.0-20240323085036-7679b4818ce7 // indirect
+	github.com/NpoolPlatform/miningpool-middleware v0.0.0-20241106020300-03d147aeb913 // indirect
 	github.com/Shonminh/apollo-client v0.4.0 // indirect
 	github.com/agext/levenshtein v1.2.1 // indirect
 	github.com/andres-erbsen/clock v0.0.0-20160526145045-9e14626cd129 // indirect
@@ -82,7 +80,6 @@ require (
 	github.com/aws/smithy-go v1.13.2 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/bmatcuk/doublestar v1.3.4 // indirect
-	github.com/bmatcuk/doublestar/v4 v4.0.2 // indirect
 	github.com/bytedance/sonic/loader v0.1.1 // indirect
 	github.com/cenkalti/backoff/v3 v3.2.2 // indirect
 	github.com/cespare/xxhash v1.1.0 // indirect
@@ -99,8 +96,7 @@ require (
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-openapi/inflect v0.19.0 // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
-	github.com/google/addlicense v1.1.1 // indirect
-	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/google/go-cmp v0.7.0 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
 	github.com/hashicorp/go-hclog v1.2.0 // indirect
@@ -136,7 +132,7 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.3.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
-	github.com/ugorji/go/codec v1.2.14 // indirect
+	github.com/ugorji/go/codec v1.3.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	github.com/zclconf/go-cty v1.14.4 // indirect
 	github.com/zclconf/go-cty-yaml v1.1.0 // indirect
@@ -147,7 +143,6 @@ require (
 	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
 	golang.org/x/mod v0.24.0 // indirect
 	golang.org/x/net v0.37.0 // indirect
-	golang.org/x/sync v0.12.0 // indirect
 	golang.org/x/sys v0.31.0 // indirect
 	golang.org/x/text v0.23.0 // indirect
 	golang.org/x/tools v0.31.0 // indirect
