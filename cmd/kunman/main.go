58848d437e3764dec61b912e2fa60dcfc0ccab73
Zhao KK <kikakkz@hotmail.com>
2025-06-21 03:07:04 +0000

    Add dependencies moduels

--- cmd/kunman/main.go
+++ cmd/kunman/main.go
@@ -17,10 +17,13 @@ import (
 	appusermwsvcname "github.com/NpoolPlatform/kunman/middleware/appuser/servicename"
 	basalservicename "github.com/NpoolPlatform/kunman/middleware/basal/servicename"
 	chainmwsvcname "github.com/NpoolPlatform/kunman/middleware/chain/servicename"
+	g11nmwsvcname "github.com/NpoolPlatform/kunman/middleware/g11n/servicename"
 	goodmwsvcname "github.com/NpoolPlatform/kunman/middleware/good/servicename"
 	inspiremwsvcname "github.com/NpoolPlatform/kunman/middleware/inspire/servicename"
 	ledgermwsvcname "github.com/NpoolPlatform/kunman/middleware/ledger/servicename"
+	notifmwsvcname "github.com/NpoolPlatform/kunman/middleware/notif/servicename"
 	ordermwsvcname "github.com/NpoolPlatform/kunman/middleware/order/servicename"
+	reviewmwsvcname "github.com/NpoolPlatform/kunman/middleware/review/servicename"
 	sphinxproxysvcname "github.com/NpoolPlatform/sphinx-proxy/pkg/servicename"
 
 	cli "github.com/urfave/cli/v2"
@@ -49,6 +52,9 @@ func main() {
 		ledgermwsvcname.ServiceDomain,
 		inspiremwsvcname.ServiceDomain,
 		ordermwsvcname.ServiceDomain,
+		g11nmwsvcname.ServiceDomain,
+		reviewmwsvcname.ServiceDomain,
+		notifmwsvcname.ServiceDomain,
 		accountmwsvcname.ServiceDomain,
 		appusermwsvcname.ServiceDomain,
 		chainmwsvcname.ServiceDomain,
