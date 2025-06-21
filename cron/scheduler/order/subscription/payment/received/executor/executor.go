eee296b2191d26aa6d2b3a68f57c5d4012d0ed68
Zhao KK <kikakkz@hotmail.com>
2025-06-21 06:21:28 +0000

    Add subscription oorder payment received

---
+++ cron/scheduler/order/subscription/payment/received/executor/executor.go
@@ -0,0 +1,28 @@
+package executor
+
+import (
+	"context"
+	"fmt"
+
+	baseexecutor "github.com/NpoolPlatform/kunman/cron/scheduler/base/executor"
+	subscriptionordermwpb "github.com/NpoolPlatform/kunman/message/order/middleware/v1/subscription"
+)
+
+type handler struct{}
+
+func NewExecutor() baseexecutor.Exec {
+	return &handler{}
+}
+
+func (e *handler) Exec(ctx context.Context, order interface{}, persistent, notif, done chan interface{}) error {
+	_order, ok := order.(*subscriptionordermwpb.SubscriptionOrder)
+	if !ok {
+		return fmt.Errorf("invalid order")
+	}
+
+	h := &orderHandler{
+		SubscriptionOrder: _order,
+		persistent:        persistent,
+	}
+	return h.exec(ctx)
+}
--- cron/scheduler/order/subscription/subscription.go
+++ cron/scheduler/order/subscription/subscription.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/NpoolPlatform/kunman/cron/scheduler/config"
 	"github.com/NpoolPlatform/kunman/cron/scheduler/order/subscription/created"
+	paymentreceived "github.com/NpoolPlatform/kunman/cron/scheduler/order/subscription/payment/received"
 	paymentwait "github.com/NpoolPlatform/kunman/cron/scheduler/order/subscription/payment/wait"
 	"github.com/NpoolPlatform/kunman/framework/logger"
 )
@@ -25,12 +26,14 @@ func Initialize(ctx context.Context, cancel context.CancelFunc) {
 
 	created.Initialize(ctx, cancel, &running)
 	paymentwait.Initialize(ctx, cancel, &running)
+	paymentreceived.Initialize(ctx, cancel, &running)
 }
 
 func Finalize(ctx context.Context) {
 	if b := config.SupportSubsystem(subsystem); !b {
 		return
 	}
+	paymentreceived.Finalize(ctx)
 	paymentwait.Finalize(ctx)
 	created.Finalize(ctx)
 }
